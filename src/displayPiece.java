02283617e4b2eb48ed42376bd8e255849d6ac7de
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-21 15:52:48 -0500

    Update displayPiece.java

--- src/displayPiece.java
+++ src/displayPiece.java
@@ -3,39 +3,23 @@ import java.awt.event.ActionListener;
 
 public class displayPiece extends HelperFunctions implements setupVars, ActionListener {
     int[] corners = {0, 10, 110, 120};
+    int[] blackRookLoc = {3, 4, 5, 6, 7,
+                          33, 44, 55, 66, 77,
+                          43, 54, 65, 76, 87,
+                          113, 114, 115, 116, 117,
+                          16, 56, 64 ,104};
+    int[] whiteRookLoc = {38,
+                          48, 49, 50,
+                          58, 59, 61, 62,
+                          70, 71, 72,
+                          82};
     public void display() {
-       for (int i = 3; i <= 7; i++) {
-            placePiece(i, blackRook, "Black Rook");
-        }
-       for (int i = 33; i <= 77; i += 11) {
-           placePiece(i, blackRook, "Black Rook");
+        placePiece(60, King, "King");
+        for (int loc : blackRookLoc) {
+           placePiece(loc, blackRook, "Black Rook");
        }
-       for (int i = 43; i <= 87; i += 11) {
-           placePiece(i, blackRook, "Black Rook");
-       }
-       for (int i = 113; i <= 117; i++) {
-           placePiece(i, blackRook, "Black Rook");
-       }
-       placePiece(16, blackRook, "Black Rook");
-       placePiece(104, blackRook, "Black Rook");
-       placePiece(56, blackRook, "Black Rook");
-       placePiece(64, blackRook, "Black Rook");
-       for (int i = 58; i <= 62; i++) {
-           if (i == 59 || i == 61) {
-               placePiece(i - 11, whiteRook, "White Rook");
-               placePiece(i, whiteRook, "White Rook");
-               placePiece(i + 11, whiteRook, "White Rook");
-           }
-           else if (i == 60) {
-               placePiece(i - 22, whiteRook, "White Rook");
-               placePiece(i - 11, whiteRook, "White Rook");
-               placePiece(i, King, "King");
-               placePiece(i + 11, whiteRook, "White Rook");
-               placePiece(i + 22, whiteRook, "White Rook");
-           }
-           else {
-               placePiece(i, whiteRook, "White Rook");
-           }
+       for (int loc : whiteRookLoc) {
+           placePiece(loc, whiteRook, "White Rook");
        }
        for (int i = 0; i < 121; i++)
            buttons[i].addActionListener(this);
@@ -51,11 +35,13 @@ public class displayPiece extends HelperFunctions implements setupVars, ActionLi
                         break;
                     }
                     else System.out.println("Pick up a piece bozo");
+                    System.out.println(pieceHeld);
                 }
                 else {
                     if (holdingPiece)
                         Move.empty(i);
                     else System.out.println("Pick up a piece bozo");
+                    System.out.println(pieceHeld);
                 }
                 for (int j = 0; j < 121; j++) {
                     if (!buttons[j].getText().equals(""))
