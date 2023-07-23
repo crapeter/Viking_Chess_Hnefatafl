85580c91616c8cfaed8b51f7859374edcf38fd21
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-23 17:43:42 -0500

    Add files via upload

---
+++ Hnefatafl/src/HelperFunctions.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HelperFunctions implements setupVars {
+    public static int location = 0;
+    public static boolean gameOver = false;
+    public static boolean holdingPiece = false;
+    public static String pieceHeld = "";
+    public static ImageIcon icon = null;
+    public static void swapPiece(int oldLoc, int newLoc, ImageIcon currentIcon, String name) {
+        pieceLoc.remove(oldLoc, name);
+        pieceLoc.put(newLoc, name);
+        buttons[oldLoc].setIcon(null);
+        buttons[newLoc].setIcon(currentIcon);
+        holdingPiece = false;
+    }
+    public static void pickUpPiece(int loc, String pieceName) {
+        pieceLoc.remove(loc, pieceName);
+        holdingPiece = true;
+    }
+    public static void placePiece(int loc, ImageIcon newIcon, String pieceName) {
+        pieceLoc.put(loc, pieceName);
+        buttons[loc].setIcon(newIcon);
+    }
+    public static void grabPiece(int loc, ImageIcon newIcon) {
+        pieceHeld = pieceLoc.get(loc);
+        location = loc;
+        icon = newIcon;
+    }
+    public static void displayMoves(int loc, JButton[] buttons, Color color) {
+        buttons[loc].setText("a");
+        buttons[loc].setForeground(color);
+    }
+}
---
+++ Hnefatafl/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Draw board = new Draw();
+        DisplayPiece piece = new DisplayPiece();
+        board.draw();
+        piece.display();
+    }
+}
