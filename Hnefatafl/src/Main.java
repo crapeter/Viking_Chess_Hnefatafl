bac6dac928af55a6b7a3e2f298bb1dad746b059a
Craig Peterson <139075832+crapeter@users.noreply.github.com>
2023-07-31 14:07:46 -0500

    Delete Hnefatafl directory

--- Hnefatafl/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Draw board = new Draw();
-        DisplayPiece piece = new DisplayPiece();
-        board.draw();
-        piece.display();
-    }
-}
